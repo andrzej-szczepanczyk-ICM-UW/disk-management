bf7898dc8776d0efbfa03fdce535ca063760a434
andrzej1naukowiec <andrzej.tenor@gmail.com>
2020-05-09 12:08:54 +0200

    Some setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import MainPage from './pages/MainPage.js'
 
-function App() {
+import Nav from './components/Nav'
+
+
+function App(props) {
+  console.log(props)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+      <div>
+        {}
+      </div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+      </Switch>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,8 @@
+import {withRouter} from 'react-router-dom'
+
+function Nav(props) {
+    console.log(props)
+    return "  "
+}
+
+export default withRouter(Nav)
